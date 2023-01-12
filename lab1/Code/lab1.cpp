0db3088c61f1803b65d18c044f31b2fc9cf7d7e3
Noah Batiuk <batiuk@ualberta.ca>
2023-01-12 15:53:07 -0700

    Thread spinup for main()

--- lab1/Code/lab1.cpp
+++ lab1/Code/lab1.cpp
@@ -1,8 +1,45 @@
 #include <stdio.h>
+#include <pthread.h>
+
+
+
+void* threadfunc (void* arg_p){}
+
+
 
 int main(int argc , char *argv[]) {
 
+    /*
+    Spawn a set of pthreads to operate on a set of matrix cells
+
+    -----
+    Input:
+    int thread_cnt      number of threads to be used
+
+    -----
+    Output:
+
+    -----
+    Example:
+    */
+
+
     printf("It works!\n");
+
+
+    int thread_cnt = 4;
+    pthread_t threads[thread_cnt];
+
+    for (int i=0; i<thread_cnt; i++) {
+        pthread_create(&threads[i], NULL, threadfunc, (void *) i);
+    }
+
+
     return 0;
 
 }
+
+
+
+
+
