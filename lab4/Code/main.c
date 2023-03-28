c0198fa1813f4e392057c1e321378ecb08720c01
Justin Boileau <justin@blindnet.io>
2023-03-28 15:46:38 -0600

    working main and better comments

--- lab4/Code/main.c
+++ lab4/Code/main.c
@@ -2,8 +2,10 @@
 
 // #include <mpi.h>
 #include <stdio.h>
-#include "Lab4_IO.h"
+#include <stdlib.h>
+#include <math.h>
 #include "timer.h"
+#include "Lab4_IO.h"
 
 #define EPSILON 0.00001
 #define DAMPING_FACTOR 0.85
@@ -18,65 +20,52 @@ int main(int argc, char* argv[])
     int i, j;
     double damp_const;
     int iterationcount = 0;
-    int collected_nodecount;
-    double *collected_r;
-    double cst_addapted_threshold;
-    double error;
     double start, end;
-    FILE *fp, *ip;
+    FILE *ip;
 
-    // Load the data and simple verification
-    if ((fp = fopen("data_output", "r")) == NULL ){
-    	printf("Error loading the data_output.\n");
-        return 253;
-    }
-    fscanf(fp, "%d\n%lf\n", &collected_nodecount, &error);
+    // Read the input file
     if ((ip = fopen("data_input_meta","r")) == NULL) {
         printf("Error opening the data_input_meta file.\n");
         return 254;
     }
     fscanf(ip, "%d\n", &nodecount);
-    if (nodecount != collected_nodecount){
-        printf("Problem size does not match!\n");
-        return 2;
-    }
     fclose(ip);
-    collected_r = malloc(collected_nodecount * sizeof(double));
-    for ( i = 0; i < collected_nodecount; ++i)
-        fscanf(fp, "%lf\n", &collected_r[i]);
-    fclose(fp);
-
-    // Adjust the threshold according to the problem size
-    cst_addapted_threshold = THRESHOLD;
     
+    // Initialize the graph
     if (node_init(&nodehead, 0, nodecount)) return 254;
-    // initialize variables
+
+
+    // Initialize r vectors, r_i(0) = 1/N
     r = malloc(nodecount * sizeof(double));
     r_pre = malloc(nodecount * sizeof(double));
     for ( i = 0; i < nodecount; ++i)
         r[i] = 1.0 / nodecount;
+    
+    // Calculate initial contribution of all nodes (D_i) with an edge to each node i
     contribution = malloc(nodecount * sizeof(double));
     for ( i = 0; i < nodecount; ++i)
         contribution[i] = r[i] / nodehead[i].num_out_links * DAMPING_FACTOR;
     damp_const = (1.0 - DAMPING_FACTOR) / nodecount;
 
-    // CORE CALCULATION
+    // Core iteration
     GET_TIME(start);
-    do{
+    do {
         ++iterationcount;
         vec_cp(r, r_pre, nodecount);
-        // update the value
+        
+        // Update r vector for this t (iteration)
         for ( i = 0; i < nodecount; ++i){
             r[i] = 0;
             for ( j = 0; j < nodehead[i].num_in_links; ++j)
                 r[i] += contribution[nodehead[i].inlinks[j]];
             r[i] += damp_const;
         }
-        // update and broadcast the contribution
+        // Update contribution of all nodes (D_i) with an edge to each node i, used
+        // in next iteration for vector r(t+1)
         for ( i=0; i<nodecount; ++i){
             contribution[i] = r[i] / nodehead[i].num_out_links * DAMPING_FACTOR;
         }
-    }while(rel_error(r, r_pre, nodecount) >= EPSILON);
+    } while(rel_error(r, r_pre, nodecount) >= EPSILON);
     
     GET_TIME(end);
     printf("Program converged at %d th iteration.\nElapsed time %f.\n", iterationcount, end-start);
